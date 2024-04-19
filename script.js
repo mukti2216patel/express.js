2aaf86c0613a182c82d6bfa5d63d6211429a99de
mukti2216patel <mukti2216patel@gmail.com>
2024-04-20 00:53:15 +0530

    null

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-console.log("Hello world")
+var data = require('./script2')
+
+console.log(data);
+
---
+++ script2.js
@@ -0,0 +1,5 @@
+var d = 2234;
+var e = 2236;
+// module.exports = d;
+console.log(d);
+module.exports = {first : d , second : e};

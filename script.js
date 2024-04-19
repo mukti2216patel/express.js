37f7c20ad21e7a006303cf37896df527d74c10ab
mukti2216patel <mukti2216patel@gmail.com>
2024-04-20 01:14:05 +0530

    null

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 
 
+var figlet = require("figlet")
+figlet("Hello World!!" , function (err,data){
+    if(err)
+    {
+        console.log("Wrong")
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+
+});

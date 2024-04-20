16a36f8ec4acd2f0d7f65deced68e302ef7efc7f
mukti2216patel <mukti2216patel@gmail.com>
2024-04-20 22:37:25 +0530

    null

--- scrip1.js
+++ scrip1.js
@@ -2,15 +2,30 @@ const express = require('express');
 const app = express();
 app.set("view engine" , "ejs");
 app.use(express.static('./public'));
+const port = 3000
+// app.get("/", function (req, res) {
+//     res.render("index" , {age:12});
+// });
 
-app.get("/", function (req, res) {
-    res.render("index" , {age:12});
+app.get("/",(req,res)=>{
+    res.send("Hello world get");
+});
+app.post("/",(req,res)=>{
+    res.send("Hello world post ");
+});
+app.put("/",(req,res)=>{
+    res.send("Hello world post ");
+});
+app.get("/index", (req, res) => {
+    console.log("Hey its index")
+    res.sendFile('public/mypage.html', { root: __dirname })
 });
 
-app.get("/contact", function (req, res) {
-    res.render("contact");
-});
+app.get("/api", (req, res) => {
+    res.json({ a: 1, b: 2, c: 3, d: 4, name: ["harry", "jerry"] })
+})
 
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
 
-
-app.listen(3000);
--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 
-const express = require('express')
-const app = express()
+// const express = require('express')
+// const app = express()
 
-app.use(function(req,res,next){
-    console.log("hello")
-    next();
-})
-app.use(function(req,res,next){
-    console.log("hello1")
-    next();
-})
-app.get("/" , function(req,res){
-    res.send("Hello122")
-})
+// app.use(function(req,res,next){
+//     console.log("hello")
+//     next();
+// })
+// app.use(function(req,res,next){
+//     console.log("hello1")
+//     next();
+// })
+// app.get("/" , function(req,res){
+//     res.send("Hello122")
+// })
 
-app.get("/profile" , function(req,res){
-    res.send("Hello bye")
-})
-app.listen(3000);
+// app.get("/profile" , function(req,res){
+//     res.send("Hello bye")
+// })
+// app.listen(3000);

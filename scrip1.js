631a8284150d9eedb54ec809fe353358f367336c
mukti2216patel <mukti2216patel@gmail.com>
2024-04-20 17:41:52 +0530

    first backend trial

---
+++ scrip1.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+app.set("view engine" , "ejs");
+app.use(express.static('./public'));
+
+app.get("/", function (req, res) {
+    res.render("index" , {age:12});
+});
+
+app.get("/contact", function (req, res) {
+    res.render("contact");
+});
+
+
+
+app.listen(3000);
